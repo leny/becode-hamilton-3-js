6f0831068f55c63e3982183350149fa822f2cd06
MathieuThiry <thiry.math@gmail.com>
2020-02-09 12:31:15 +0100

    Added informations about Hungarian notation

--- 09-misc/03-worst-ui-one/script.js
+++ 09-misc/03-worst-ui-one/script.js
@@ -11,6 +11,17 @@
 
 (() => {
 
+    /*
+        Use of Hungarian notation (https://en.wikipedia.org/wiki/Hungarian_notation)
+
+        The main idea is to prefix variables names to see directly the type of the variable as :
+
+            const $target = dom element (<div id="target"></div>)
+            const bIsTrue = boolean (true / false)
+            const iAge = integer (25)
+
+    */
+
     const $target = document.getElementById('target');
     const $slider = document.getElementById('slider');
 
